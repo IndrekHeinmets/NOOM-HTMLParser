639e45dfa3d551406a429f9e2b7a1c31b4c0e38a
Indrek <inzaheinmets@gmail.com>
2023-07-07 14:14:55 +0100

    functional implementatio

--- parser.py
+++ parser.py
@@ -2,32 +2,36 @@ from bs4 import BeautifulSoup
 import csv
 import os
 
-folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'input_data')
-out_filename = 'output.csv'
+input_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'input_data')
+output_filename = 'output.csv'
 
-for filename in os.listdir(folder_path):
-    if filename.endswith('.htm'):
-        file_path = os.path.join(folder_path, filename)
 
-        with open(file_path, 'r', encoding='utf-8') as f:
-            content = f.read()
+def read_htm(path):
+    with open(path, 'r', encoding='utf-8') as f:
+            return f.read()
+    
 
-        limits, entries = [], []
-        raw_entries = BeautifulSoup(content, 'html.parser').find_all('div', class_='S2')
-        start_indexes = [i for i, entry in enumerate(raw_entries) if entry.text.strip() == 'Trükitud']
-        end_indexes = [i for i, entry in enumerate(raw_entries) if entry.text.strip() == 'Lk.']
+def limit_entries(raw_entries, start_lim: str, end_lim: str):
+    limits, entries = [], []
+    start_indexes = [i for i, entry in enumerate(raw_entries) if entry.text.strip() == start_lim]
+    end_indexes = [i for i, entry in enumerate(raw_entries) if entry.text.strip() == end_lim]
 
-        for start_index in start_indexes:
+    for start_index in start_indexes:
             end_index = next((i for i in end_indexes if i > start_index), None)
             if end_index is not None:
                 limits.append((start_index + 1, end_index - 3))
 
-        for limits in limits:
-            start_index, end_index = limits
-            entries.extend(raw_entries[start_index:end_index])
+    for limits in limits:
+        start_index, end_index = limits
+        entries.extend(raw_entries[start_index:end_index])
+    return entries
 
-        data = []
-        for i in range(0, len(entries), 7):
+
+def parse_htm(content, data):
+    raw_entries = BeautifulSoup(content, 'html.parser').find_all('div', class_='S2')
+    entries = limit_entries(raw_entries, start_lim='Trükitud', end_lim='Lk.')
+
+    for i in range(0, len(entries), 7):
             try:
                 nimetus = entries[i].text.strip().replace(",", ".")
                 seeria = entries[i+1].text.strip().replace(",", ".")
@@ -41,10 +45,26 @@ for filename in os.listdir(folder_path):
                             'AEGUMINE': str(aegumine)})
             except IndexError:
                 continue
+    return data
 
-with open(out_filename, 'w', newline='', encoding='utf-8') as f:
-    writer = csv.DictWriter(f, fieldnames=list(data[0].keys()))
-    writer.writeheader()
-    writer.writerows(data)
 
-print(f"Data extracted and saved to {out_filename} successfully.")
+def write_output(filename, data):
+    with open(filename, 'w', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames=list(data[0].keys()))
+        writer.writeheader()
+        writer.writerows(data)
+    print(f'Data extracted & saved to {filename} successfully!')
+
+
+def main():
+    data = []
+    for filename in os.listdir(input_folder_path):
+        if filename.endswith('.htm'):
+            file_path = os.path.join(input_folder_path, filename)
+            data.append({'NIMETUS': '_', 'SEERIA': '_', 'HIND': str(filename), 'LAOJAAK': '_', 'AEGUMINE': '_'})
+            data = parse_htm(read_htm(file_path), data)
+    write_output(output_filename, data)
+
+
+if __name__ == '__main__':
+     main()
