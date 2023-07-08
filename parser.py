ba51308084a0134bedec40a603397e529b14ae7c
Indrek <inzaheinmets@gmail.com>
2023-07-08 12:25:48 +0100

    error handling & strong type checks

--- parser.py
+++ parser.py
@@ -1,17 +1,16 @@
 from bs4 import BeautifulSoup
-import csv
-import os
+import csv, os
 
 input_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'input_data')
 output_filename = 'output.csv'
 
 
-def read_htm(path):
+def read_htm(path: str):
     with open(path, 'r', encoding='utf-8') as f:
             return f.read()
     
 
-def limit_entries(raw_entries, start_lim: str, end_lim: str):
+def limit_entries(raw_entries: list, start_lim: str, end_lim: str):
     limits, entries = [], []
     start_indexes = [i for i, entry in enumerate(raw_entries) if entry.text.strip() == start_lim]
     end_indexes = [i for i, entry in enumerate(raw_entries) if entry.text.strip() == end_lim]
@@ -27,7 +26,7 @@ def limit_entries(raw_entries, start_lim: str, end_lim: str):
     return entries
 
 
-def parse_htm(content, data):
+def parse_htm(content: list, data: list, filename: str, error: bool):
     raw_entries = BeautifulSoup(content, 'html.parser').find_all('div', class_='S2')
     entries = limit_entries(raw_entries, start_lim='Trükitud', end_lim='Lk.')
 
@@ -43,27 +42,38 @@ def parse_htm(content, data):
                             'HIND': float(hind),
                             'LAOJAAK': float(laojaak),
                             'AEGUMINE': str(aegumine)})
-            except IndexError:
+            except IndexError as Ie:
+                print(f'\nFailed to parse line {i} in "{filename}": {Ie}')
+                error = True
                 continue
-    return data
+            except ValueError as Ve:
+                print(f'\nFailed to parse line {i} in "{filename}": {Ve}')
+                error = True
+                continue
+            except Exception as Ex:
+                print(f'\nError occured in "{filename}": {Ex}')
+                error = True
+                break
+    return data, error
 
 
-def write_output(filename, data):
+def write_output(filename: str, data: list):
     with open(filename, 'w', newline='', encoding='utf-8') as f:
         writer = csv.DictWriter(f, fieldnames=list(data[1].keys()))
         writer.writeheader()
         writer.writerows(data)
-    print(f'Data extracted & saved to {filename} successfully!')
+    print(f'\nData extracted & saved to "{filename}" successfully!')
 
 
 def main():
-    data = []
+    error, exts, data = False, ['.htm', '.html'], []
     for filename in os.listdir(input_folder_path):
-        if filename.endswith('.htm'):
+        if any(filename.endswith(ext) for ext in exts):
             file_path = os.path.join(input_folder_path, filename)
             data.append({'NIMETUS': str(filename[:-4])})
-            data = parse_htm(read_htm(file_path), data)
-    write_output(output_filename, data)
+            data, error = parse_htm(read_htm(file_path), data, filename, error)
+    if not error:
+        write_output(output_filename, data)
 
 
 if __name__ == '__main__':
